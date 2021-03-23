c628bda5fa911399882ab969e7a38e6712322427
Hunachi <hanahanahunachi@gmail.com>
2021-03-24 02:47:05 +0900

    Modify boot flag code.

--- src/03_boot_putc/boot.s
+++ src/03_boot_putc/boot.s
@@ -1,15 +1,15 @@
 ; Reference: 作って理解するOS x86系コンピュータを動かす理論と実装 (https://gihyo.jp/book/2019/978-4-297-10847-2)
 
-    BOOT_LOAD   equ     0x7C00 ; Address of boot program
+    BOOT_LOAD equ 0x7C00 ; Address of boot program
 
-    ORG     BOOT_LOAD          ; Instract load address to assembler.
+    ORG BOOT_LOAD        ; Instract load address to assembler.
 
 ; *****************
 ;    Entory Point 
 ; *****************
 entry:
     jmp     ipl
-    times   90 - ( $ - $$ ) db 0x90 ; BPB(BOPS Parameter Block)
+    times    90-($-$$) db 0x90 ; BPB(BOPS Parameter Block)
 
 ipl:
     cli     ; Disabling Inerrupt
@@ -24,6 +24,11 @@ ipl:
 
     mov     [BOOT.DRIVE], dl
 
+    mov     al, 'A'     ; AL = output word
+    mov     ah, 0x0E    ; teletype's single word output
+    mov     bx, 0x0000  ; setting page number and word's color 0
+    int     0x10        ; call video BIOS.
+
     jmp     $
 
 ALIGN 2, db 0
@@ -32,4 +37,3 @@ BOOT:
 
     times    510-($-$$) db 0x00
     db      0x55, 0xAA
-
