58d6f60a222dbc6aaf83dfaf10eca13fc906b381
Hunachi <hanahanahunachi@gmail.com>
2021-05-01 18:48:37 +0900

    rm files

--- src/include/define.s
+++ src/include/define.s
@@ -5,3 +5,6 @@ SECT_SIZE   equ     (512)
 BOOT_SECT   equ     (BOOT_SIZE / SECT_SIZE) ; Sector count of Boot program. 
 
 E820_RECORD_SIZE    equ     20
+
+KERNEL_LOAD equ     0x0010_1000
+KERNEL_SIZE equ     (1024 * 8)
