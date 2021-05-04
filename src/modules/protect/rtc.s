f960e30bb444ec28ba918cf2bd94a7dc0c003eb8
Hunachi <hanahanahunachi@gmail.com>
2021-05-05 04:29:10 +0900

    implement 26

---
+++ src/modules/protect/rtc.s
@@ -0,0 +1,45 @@
+; get current time
+rtc_get_time:
+    push    ebp
+    mov     ebp, esp
+
+    push    ebx
+
+    ; set (0:sec, 2:min, 4:hour, 7:day, 8:month, 9:year) to 0x07
+    ; and then get time from 0x71
+    mov     al, 0x0A
+    out     0x70, al
+    in      al, 0x71
+    test    al, 0x80            ; refreshing..(DM & UIP)
+    je      .10F
+    mov     eax, 1
+    jmp     .10E
+.10F
+    mov     al, 0x04
+    out     0x07, al
+    in      al, 0x71
+
+    shl     eax, 8
+
+    mov     al, 0x02
+    out     0x07, al
+    in      al, 0x71
+
+    shl     eax, 8
+
+    mov     al, 0x00
+    out     0x07, al
+    in      al, 0x71
+
+    and     eax, 0x00_FF_FF_FF  ; lower 3 byte is usable
+
+    mov     ebx, [ebp + 8]
+    mov     [ebx], eax          ; return data
+.10E:
+    pop     ebx
+
+    mov     esp, ebp
+    pop     ebp
+
+    ret
+
