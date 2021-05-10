be37ff1a27b2a65396b526e8b3a7121e976a5ef5
Hunachi <hanahanahunachi@gmail.com>
2021-05-10 14:24:42 +0900

    implement 40 paging

---
+++ src/40_paging/modules/paging.s
@@ -0,0 +1,49 @@
+; Create a page table
+page_set_4m:
+
+    push    ebp
+    mov     ebp, esp
+
+    pusha
+
+    ; Create page directory (P=0)
+    mov     edi, [ebp + 8]      ; Head of page directory
+    mov     eax, 0x00000000     ; P=0
+    mov     ecx, 1024
+    rep stosd                   ; while(ecx--) *dst++ = attribute
+
+    ; Setting the head entry
+    mov     eax, edi
+    and     eax, ~0x0000_0FFF   ; Physical address
+    or      eax, 7
+    mov     [edi - (1024 * 4)], eax
+
+    ; Setting the page table
+    mov     eax, 0x0000007
+    mov     ecx, 1024
+
+.10L:
+    stosd
+    add     eax, 0x00001000
+    loop    .10L
+
+    popa
+
+    mov     esp, ebp
+    pop     ebp
+
+    ret
+
+
+init_page:
+    
+    pusha
+
+    cdecl   page_set_4m, CR3_BASE
+
+    popa
+    
+    ret
+
+
+
